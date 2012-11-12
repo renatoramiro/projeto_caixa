caeed09768e4574b07099120972203545c967ac1
Renato Ramiro <renato.ipj@gmail.com>
2012-11-12 17:27:08 -0300

    Consertei resgistration controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,9 +1,9 @@
 class RegistrationsController < Devise::RegistrationsController
-	layout 'dashboard'
 
   protected
 
     def after_update_path_for(resource)
+    	layout 'dashboard'
       information_index_path(resource)
     end
 end
